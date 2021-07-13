24abe65ce9a9347eb2a1f100434a7e87d585765f
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-07-13 15:47:09 -0300

    Estilizando Dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore respositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore respositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/60242867?s=400&u=65ef7eb344624f570e047c0d55bd8ffe32b85f7f&v=4" alt="Luiz Timbó" />
+
+          <div>
+            <strong>fsdlçkfsldkflçs</strong>
+            <p>asdsakjdlaskdksalçdkaçsl</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/60242867?s=400&u=65ef7eb344624f570e047c0d55bd8ffe32b85f7f&v=4" alt="Luiz Timbó" />
+
+          <div>
+            <strong>fsdlçkfsldkflçs</strong>
+            <p>asdsakjdlaskdksalçdkaçsl</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/60242867?s=400&u=65ef7eb344624f570e047c0d55bd8ffe32b85f7f&v=4" alt="Luiz Timbó" />
+
+          <div>
+            <strong>fsdlçkfsldkflçs</strong>
+            <p>asdsakjdlaskdksalçdkaçsl</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
