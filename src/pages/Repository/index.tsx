5bee0b899154431d9ef9bb4fd935aff339abe6a1
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-07-14 11:55:09 -0300

    Estilizando detalhes.

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
+
+import logoImg from "../../assets/logo.svg";
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,45 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h2>Repository: {params.repository} </h2>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" />
+          <div>
+            <strong></strong>
+            <p></p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong></strong>
+            <span></span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dfsdfsdf">
+          <div>
+            <strong></strong>
+            <p></p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
