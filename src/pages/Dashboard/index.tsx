8dcc296397a72be73e9bc63ebd1c0aca6c364bc0
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-07-14 10:50:48 -0300

    Salvando no localStorage.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, useEffect, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import api from "../../services/api";
 
@@ -18,7 +18,15 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState("");
   const [inputError, setInputError] = useState("");
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem("@GitHubExplorer: repositories");
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    } else {
+      return [];
+    }
+  });
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -40,6 +48,10 @@ const Dashboard: React.FC = () => {
       setInputError("Erro na busca por esse repositório.");
     }
   }
+
+  useEffect(() => {
+    localStorage.setItem("@GitHubExplorer: repositories", JSON.stringify(repositories));
+  }, [repositories]);
 
   return (
     <>
